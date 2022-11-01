f12345213b6f564cd6c99898c1d80c208aab229f
vitalii.dudok <vitalii.dudok@kindgeek.com>
2022-11-01 03:00:13 +0200

    Initial commit

---
+++ meeting-registration/src/main/java/com/sdarm/meetingregistration/MeetingRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.sdarm.meetingregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingRegistrationApplication.class, args);
+    }
+
+}
---
+++ meeting-registration/src/test/java/com/sdarm/meetingregistration/MeetingRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdarm.meetingregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
