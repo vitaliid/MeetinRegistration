94ceda0f33a71599a045f1565000bf4292a1962a
vitalii.dudok <vitalii.dudok@kindgeek.com>
2022-11-16 03:20:47 +0200

    Added cross origin annotation

--- src/main/java/com/sdarm/meetingregistration/controller/ApartmentController.java
+++ src/main/java/com/sdarm/meetingregistration/controller/ApartmentController.java
@@ -6,12 +6,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/registration/apartments")
 @OpenAPIDefinition(tags = {@Tag(name = "registration"), @Tag(name = "apartments")})
--- src/main/java/com/sdarm/meetingregistration/controller/ParticipantController.java
+++ src/main/java/com/sdarm/meetingregistration/controller/ParticipantController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/registration/participants")
 @OpenAPIDefinition(tags = {@Tag(name = "registration"), @Tag(name = "participants")})
