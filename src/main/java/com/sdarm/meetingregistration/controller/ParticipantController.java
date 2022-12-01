13617dffa15ebd2e18b93a09f88a4686b6b3f797
vitalii.dudok <vitalii.dudok@kindgeek.com>
2022-12-01 23:32:52 +0200

    extended cross-origin policy

--- src/main/java/com/sdarm/meetingregistration/controller/ParticipantController.java
+++ src/main/java/com/sdarm/meetingregistration/controller/ParticipantController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("api/registration/participants")
 @OpenAPIDefinition(tags = {@Tag(name = "registration"), @Tag(name = "participants")})
